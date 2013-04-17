2c4cc860d96103c64d862b5246b6cd39de162337
Kyle <kylekarpack@gmail.com>
2013-04-17 13:15:27 -0700

    Cleaned up, created database tables
    Foreign keys syntax still off, needs a little help

--- wp-survey-toolbox.php
+++ wp-survey-toolbox.php
@@ -25,6 +25,64 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+// Include Functions
+function toolbox_install() {
+	global $wpdb;
+	
+	//************Create Tables***************
+	
+	// Settings table
+	$create_tbl_settings = "CREATE TABLE " . $wpdb->prefix . "wp_survey_toolbox_settings (
+	  setting_id mediumint(9) NOT NULL AUTO_INCREMENT,
+	  PRIMARY KEY  (setting_id)
+	);";
+	
+	// Questions table
+	$create_tbl_questions = "CREATE TABLE " . $wpdb->prefix . "wp_survey_toolbox_questions (
+	  qid mediumint(9) NOT NULL AUTO_INCREMENT,
+	  question text NOT NULL,
+	  type text NOT NULL,
+	  answers text NOT NULL,
+	  PRIMARY KEY  (qid)
+	);";
+	
+	// Surveys table
+	$create_tbl_surveys = "CREATE TABLE " . $wpdb->prefix . "wp_survey_toolbox_surveys (
+	  sid mediumint(9) NOT NULL AUTO_INCREMENT,
+	  title text NOT NULL,
+	  PRIMARY KEY  (sid)
+	);";
+	
+	//FOREIGN KEYING FOR THESE TWO!!!
+	  //FOREIGN KEY (sid) REFERENCES " . $wpdb->prefix . "wp_survey_toolbox_surveys(sid),
+	  //FOREIGN KEY (qid) REFERENCES " . $wpdb->prefix . "wp_survey_toolbox_questions(qid)
+	
+	// Responses table
+	$create_tbl_responses = "CREATE TABLE " . $wpdb->prefix . "wp_survey_toolbox_responses (
+	  rid mediumint(9) NOT NULL AUTO_INCREMENT,
+	  sid mediumint(9) NOT NULL,
+	  qid mediumint(9) NOT NULL,
+	  value text,
+	  PRIMARY KEY  (rid)
+	);";
+	
+	// Lookup table
+	$create_tbl_lookup = "CREATE TABLE " . $wpdb->prefix . "wp_survey_toolbox_lookup (
+	  sid mediumint(9) NOT NULL,
+	  qid mediumint(9) NOT NULL
+	);";
+	
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta($create_tbl_settings);
+	dbDelta($create_tbl_questions);
+	dbDelta($create_tbl_surveys);
+	dbDelta($create_tbl_responses);
+	dbDelta($create_tbl_lookup);
+}
+
+// Add the data tables
+register_activation_hook( __FILE__, 'toolbox_install' );
+
 // Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
 	echo 'It was worth a shot, but you can\'t access survey functionality this way. Sorry!';
@@ -35,12 +93,13 @@ add_action('admin_menu', 'surveytoolbox_menu');
 function surveytoolbox_menu() {
 	// Add the top main menu item in the admin sidebar
 	if (function_exists('add_menu_page')) {
-		add_menu_page('Survey Toolbox', 'Survey Toolbox', 'activate_plugins', 'survey/wp-survey-toolbox.php', '', plugins_url('survey/images/icon.png'), 100);
+		add_menu_page('Survey Toolbox', 'Survey Toolbox', 'activate_plugins', 'wp-survey-toolbox/wp-survey-toolbox.php', '', plugins_url('wp-survey-toolbox/images/icon.png'), 100);
 	}
 	// Add submenu items
 	if (function_exists('add_submenu_page')) {
-		add_submenu_page('survey/wp-survey-toolbox.php', 'Manage Surveys', 'Manage Surveys', 'activate_plugins', 'survey/survey-builder.php');
-		add_submenu_page('survey/wp-survey-toolbox.php', 'See Results', 'See Results', 'activate_plugins', 'survey/results.php');
+		add_submenu_page('wp-survey-toolbox/wp-survey-toolbox.php', 'Manage Surveys', 'Manage Surveys', 'activate_plugins', 'wp-survey-toolbox/survey-builder.php');
+		add_submenu_page('wp-survey-toolbox/wp-survey-toolbox.php', 'See Results', 'See Results', 'activate_plugins', 'wp-survey-toolbox/survey-builder.php');
+		//add_submenu_page('wp-survey-toolbox/wp-survey-toolbox.php', 'See Results', 'See Results', 'activate_plugins', 'wp-survey-toolbox/results.php');
 	}
 }
 	
