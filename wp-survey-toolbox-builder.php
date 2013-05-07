79dbcb7ef4bee78109b4b5b76cfa3a83b60f0372
Kyle <kylekarpack@gmail.com>
2013-05-07 16:43:02 -0700

    Global added STBroot

--- wp-survey-toolbox-builder.php
+++ wp-survey-toolbox-builder.php
@@ -1,5 +1,10 @@
 <link type="text/css" rel="stylesheet" media="screen" href="<?php echo(plugins_url( 'css/build-survey.css', __FILE__ ));?> ">
+<script>
+	var STBroot = "<?php echo plugins_url() . "/wp-survey-toolbox/"; ?>";
+	console.log(STBroot);
+</script>
 <script data-main="<?php echo(plugins_url( '/js/init/initSurveyBuild.js', __FILE__ )); ?>" src="<?php echo(plugins_url( '/js/libs/require.js', __FILE__ ));?>"></script>
+
 <body>
     <div id="container">
         <div id="question-container">
