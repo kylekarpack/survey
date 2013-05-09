cbe9e315b9326cf6d03a21531bd788700f689973
Kyle <kylekarpack@gmail.com>
2013-05-09 16:33:41 -0700

    Manager updates

--- wp-survey-toolbox-builder.php
+++ wp-survey-toolbox-builder.php
@@ -1,7 +1,6 @@
 <link type="text/css" rel="stylesheet" media="screen" href="<?php echo(plugins_url( 'css/build-survey.css', __FILE__ ));?> ">
 <script>
 	var STBroot = "<?php echo plugins_url() . "/wp-survey-toolbox/"; ?>";
-	console.log(STBroot);
 </script>
 <script data-main="<?php echo(plugins_url( '/js/init/initSurveyBuild.js', __FILE__ )); ?>" src="<?php echo(plugins_url( '/js/libs/require.js', __FILE__ ));?>"></script>
 
