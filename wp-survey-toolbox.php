3b106e32984bb571713fc3fbdca66ed57dc01da5
Kyle <kylekarpack@gmail.com>
2013-05-09 16:19:13 -0700

    fixed

--- wp-survey-toolbox.php
+++ wp-survey-toolbox.php
@@ -76,7 +76,7 @@ function toolbox_install() {
 	// Lookup table
 	$create_tbl_lookup = "CREATE TABLE " . $wpdb->prefix . "wp_survey_toolbox_lookup (
 	  sid mediumint(9) NOT NULL,
-	  qid mediumint(9) NOT NULL
+	  qid mediumint(9) NOT NULL,
 	  index mediumint(9) NOT NULL
 	);";
 	
