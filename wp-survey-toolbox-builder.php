f5ac3384d5c880d329ab55ed03301b78ef907c11
Kyle <kylekarpack@gmail.com>
2013-05-13 20:54:10 -0700

    Again

--- wp-survey-toolbox-builder.php
+++ wp-survey-toolbox-builder.php
@@ -1,17 +1,21 @@
 <?php
 global $current_user; ?>
 
-<link type="text/css" rel="stylesheet" media="screen" href="<?php echo(plugins_url( 'css/build-survey.css', __FILE__ ));?> ">
+<link type="text/css" rel="stylesheet" media="screen" href="<?php echo(plugins_url( 'css/survey_build.css', __FILE__ ));?> ">
 <script>
 	var STBroot = "<?php echo plugins_url() . "/wp-survey-toolbox/"; ?>";
 	var author = "<?php echo wp_get_current_user()->user_login ?>";
 </script>
-<script data-main="<?php echo(plugins_url( '/js/init/initSurveyBuild.js', __FILE__ )); ?>" src="<?php echo(plugins_url( '/js/libs/require.js', __FILE__ ));?>"></script>
+<script data-main="<?php echo(plugins_url( '/js/init/init_survey_build.js', __FILE__ )); ?>" src="<?php echo(plugins_url( '/js/libs/require.js', __FILE__ ));?>"></script>
+
 
 <body>
     <div id="container">
+        <div id="survey-toolbox-toolbar">
+            <button id="save-survey" type="button">Save</button>
+            <span class="stb-title">Currently editing...</span>
+        </div>
         <div id="question-container">
         </div>
         <button id="add-question" type="button">New Question</button>
-        <button id="save-survey" type="button">Save</button>
     </div>
--- wp-survey-toolbox-manager.php
+++ wp-survey-toolbox-manager.php
@@ -1,10 +1,10 @@
-<link type="text/css" rel="stylesheet" media="screen" href="<?php echo(plugins_url( 'css/layout.css', __FILE__ ));?> ">
+<link type="text/css" rel="stylesheet" media="screen" href="<?php echo(plugins_url( 'css/survey.css', __FILE__ ));?> ">
 <script>	
 	var STBroot = "<?php echo plugins_url() . "/wp-survey-toolbox/"; ?>";
 	console.log(STBroot);
-	console.log("<?php echo(plugins_url( '/js/init/initSurvey.js', __FILE__ )); ?>");
+	console.log("<?php echo(plugins_url( '/js/init/init_survey.js', __FILE__ )); ?>");
 </script>
-<script data-main="<?php echo(plugins_url( '/js/init/initSurveyManager.js', __FILE__ )); ?>" src="<?php echo(plugins_url( '/js/libs/require.js', __FILE__ ));?>"></script>
+<script data-main="<?php echo(plugins_url( '/js/init/init_survey_manager.js', __FILE__ )); ?>" src="<?php echo(plugins_url( '/js/libs/require.js', __FILE__ ));?>"></script>
 
 <body>
     <div id="container">
